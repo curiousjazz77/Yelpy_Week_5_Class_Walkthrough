c150815aaf10df2131248f3d17d2382496cc060f
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-17 13:30:36 -0500

    initialize app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yelpy
+iOS App to be used to teach for CodePath students
+# Yelpy_Week_5_Class_Walkthrough
---
+++ Yelpy/Controllers/Chat/LoginViewController.swift
@@ -0,0 +1,117 @@
+//
+//  LoginViewController.swift
+//  Yelpy
+//
+//  Created by Memo on 7/1/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    /*------ Outlets ------*/
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    /*------ Handle text field inputs  ------*/
+    func usernameAndPasswordNotEmpty() -> Bool {
+        return usernameTextField.text!.isEmpty || passwordTextField.text!.isEmpty
+    }
+    /*------ SIGN UP AND LOG IN FUNCTIONALITY  ------*/
+    
+    // Sign up error alert controller
+    func displaySignupError(error: Error) {
+        let title = "Sign up error"
+        let message = "Oops! Something went wrong while signing up: \(error.localizedDescription)"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    // ––––– LAB 5 TODO: SIGN UP FUNCTIONALITY
+    @IBAction func onSignUp(_ sender: Any) {
+        // Sign up user
+        // Check text field inputs
+        if !usernameAndPasswordNotEmpty(){
+            // initialize a user object
+            let newUser = PFUser()
+            
+            // set user properties
+            newUser.username = usernameTextField.text
+            newUser.password = passwordTextField.text
+         
+            
+            // call sign up function on the object
+            newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.displaySignupError(error: error)
+            } else {
+               print("User \(newUser.username!) Registereed successfully")
+                NotificationCenter.default.post(name: NSNotification.Name("login"), object: nil)
+            }
+            }
+        } else {
+            displayError()
+        }
+    }
+    
+    
+    // ––––– LAB 5 TODO: LOGIN FUNCTIONALITY
+    @IBAction func onLogin(_ sender: Any) {
+        // Login user
+    
+        
+    }
+    
+    
+    
+    
+    
+    
+    /*------ Alert Controllers ------*/
+    // Text fields are empty alert controller
+    func displayError() {
+        let title = "Error"
+        let message = "Username and password field cannot be empty"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    // Login error alert controller
+    func displayLoginError(error: Error) {
+        let title = "Login Error"
+        let message = "Oops! Something went wrong while logging in: \(error.localizedDescription)"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    
+    
+    
+}
+
+
+
